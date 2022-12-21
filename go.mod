6ba1cfda6d464880bab62cc010286e7e3beb567a
danztran <nguyenbao1917@gmail.com>
2022-12-21 17:03:10 +0700

    init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/danztran/jwt
+
+go 1.17
+
+require github.com/spf13/cast v1.5.0
---
+++ main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+	"time"
+
+	"github.com/spf13/cast"
+)
+
+func main() {
+	token := getToken()
+	if token != "" {
+		decodeToken(token)
+	}
+}
+
+func decodeToken(token string) {
+	token = strings.TrimSpace(token)
+	token = strings.TrimPrefix(token, "Bearer ")
+	token = strings.TrimPrefix(token, "bearer ")
+	token = strings.TrimSpace(token)
+
+	parts := strings.Split(token, ".")
+
+	for _, part := range parts {
+		data, err := base64.RawStdEncoding.DecodeString(part)
+		if err != nil {
+			continue
+		}
+
+		var meta map[string]interface{}
+		if err := json.Unmarshal(data, &meta); err != nil {
+			continue
+		}
+
+		dict := make(map[string]string)
+		keys := make(sort.StringSlice, 0, len(dict))
+		for k, v := range meta {
+			keys = append(keys, k)
+			var s string
+			switch k {
+			case "exp", "nbf", "iat":
+				num := cast.ToInt64(v)
+				ts := time.Unix(num, 0)
+				s = fmt.Sprintf("%s: %d (%s)", k, num, ts.Format(time.RFC3339))
+			default:
+				s = fmt.Sprintf("%s: %v", k, v)
+			}
+			dict[k] = s
+		}
+		keys.Sort()
+
+		for _, k := range keys {
+			fmt.Println(dict[k])
+		}
+	}
+}
+
+func getToken() string {
+	d := os.Stdin
+	defer d.Close()
+	s, err := io.ReadAll(d)
+	if err != nil {
+		panic(err)
+	}
+	return string(s)
+}
